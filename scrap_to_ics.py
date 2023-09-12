a8ad0089737d49f1dc7d002556d7541b8ae8fdc8
Nexgear <a.fgx2004@gmail.com>
2023-09-12 11:48:49 +0200

    Scrap_to_ics

---
+++ scrap_to_ics.py
@@ -0,0 +1,13 @@
+from icalendar import Calendar, Event, vCalAddress, vText, Alarm
+from datetime import datetime, timedelta
+from pathlib import Path
+import pytz
+import os
+
+def scrap_to_ics(title, group, year, month, hour, minute, course, organizer):
+
+
+
+
+
+    return
